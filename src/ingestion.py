d5103b3935dd17f1c442ef8e45b20fcb8973405d
lucia-rr <texedu01@gmail.com>
2026-07-26 12:12:48 -0400

    Update ingestion.py
    Eliminacion de lineas comentadas

--- src/ingestion.py
+++ src/ingestion.py
@@ -5,21 +5,21 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_cohere import CohereEmbeddings
 from langchain_community.vectorstores import FAISS
 
-# Cargamos variables de entorno para la API KEY
+
 load_dotenv()
 
-# 1. Definimos las rutas
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(base_dir, "..", "data")
 db_path = os.path.join(base_dir, "..", "db", "faiss_index")
 
-# Archivos PDF de BimBam Buy
+
 pdfs = [
     "pf_metodo_pago.pdf",
     "manual_garantia.pdf"
 ]
 
-# 2. Cargamos todos los PDFs
+
 print("📄 Cargando documentos de BimBam Buy...")
 documentos = []
 for pdf in pdfs:
@@ -30,13 +30,13 @@ for pdf in pdfs:
 
 print(f"✅ Total de páginas cargadas: {len(documentos)}")
 
-# 3. Dividimos el texto en fragmentos
+
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200
 )
 documentos_divididos = text_splitter.split_documents(documentos)
-print(f"📦 Documento dividido en {len(documentos_divididos)} fragmentos.")
+print(f"Documento dividido en {len(documentos_divididos)} fragmentos.")
 
 # 4. CONFIGURACIÓN COHERE
 print("🧠 Generando embeddings con Cohere...")
